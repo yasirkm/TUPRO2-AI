73829474cf589b857675a784c6b76966a240f9cc
Yasir Khairul Malik <yasirkm3071@gmail.com>
2022-05-20 16:58:16 +0700

    added comments and cleaned up the code

--- fuzzy.py
+++ fuzzy.py
@@ -1,48 +1,59 @@
 from openpyxl import Workbook, load_workbook
 from pathlib import Path
 
+# Paths for file processing
 FOLDER_PATH = Path(__file__).parent
 EXCEL_PATH = FOLDER_PATH/"bengkel.xlsx"
 OUTPUT_PATH = FOLDER_PATH/"top10.xlsx"
 
 def main():
-    # Load excel file
+    # Loading bengkel.xlsx
     wb = load_workbook(EXCEL_PATH)
     ws = wb["Sheet1"]
-    service = {"buruk": 0, "biasa": 0, "bagus": 0}
-    price   = {"murah": 0, "sedang": 0,"mahal": 0}
+
+    # Getting data
     rows = ws.rows
     next(rows)
 
+    # Creating Bengkel instance from data
     bengkel_list = [Bengkel(id.value, service.value, price.value) for id, service, price in rows]
+    # Sorting Bengkel instance based on its score
     bengkel_list.sort(key=lambda x: x.score, reverse=True)
     
-    for bengkel in bengkel_list:
-        print(bengkel.score)
+    # Closing excel file
     wb.close()
 
+    # Creating output file
     wb = Workbook()
     ws = wb.active
-    ws.append(("id", "servis", "harga"))
+
+    # Appending data to output file
+    ws.append(("id", "servis", "harga", "Skor"))
     for bengkel in bengkel_list[:10]:
         ws.append(bengkel.get_data())
+    
+    # Saving data in output file
     wb.save(OUTPUT_PATH)
+
+    # Closing output file
     wb.close()
 
-    #ws.cell(row=1,col)
-    #for row, 
-
 class Bengkel():
+    '''Object representation of bengkel data'''
     def __init__(self, id, service, price):
+        '''Initialize Bengkel instance'''
         self.id = id
         self.service = service
         self.price = price
 
+        # Calculating instance's score using fuzzy logic
         fuzzy_value = self.fuzzification()
         fuzzy_value = self.inference(*fuzzy_value)
         self.score = self.defuzzification(fuzzy_value)
 
     def fuzzification(self):
+        '''returns a dictionary of fuzzy values calculated using instance's service and value as crisp value'''
+
         fuzzy_service = {}
         fuzzy_price = {}
         #MEMBERSHIP FUNCTION (CRISP INPUT -> FUZZY INPUT)
@@ -58,7 +69,9 @@ class Bengkel():
         return fuzzy_service, fuzzy_price
 
     def inference(self, fuzzy_service, fuzzy_price):
-        '''inference rules '''
+        '''Returns a dictionary of fuzzy output from a dictionary of fuzzy input based on inference table'''
+
+        # Inference table initialization
         inference_table = {
             "buruk": {
                 "murah":"cukup",
@@ -76,11 +89,15 @@ class Bengkel():
                 "mahal":"cukup"
             }    
         }
+
+        # Fuzzy output initialization
         fuzzy_score = {
             "baik":0,
             "cukup":0,
             "buruk":0
         }
+
+        # Fuzzy output calculation
         for service_key in fuzzy_service:
             for price_key in fuzzy_price:
                 score_key = inference_table[service_key][price_key]
@@ -90,8 +107,11 @@ class Bengkel():
         return fuzzy_score
 
     def defuzzification(self, fuzzy_score):
+        '''Returns score as crisp value from its fuzzy value'''
+        # divident and divisor initialization
         dividend = 0
         divisor = 0
+        # cumming dividends and divisors
         for x in range(10,101,10):
             thres_buruk = min(fuzzy_score["buruk"],((50-x)/(50-0)))
             thres_cukup = min((x-25)/(50-25), (75-x)/(75-50))
@@ -107,11 +127,13 @@ class Bengkel():
             dividend +=  x * score_thres
             divisor += score_thres
 
+        # returning score
         score = dividend/divisor
         return score
     
     def get_data(self):
-        return self.id, self.service, self.price
+        '''Returns data to be appended to output file'''
+        return self.id, self.service, self.price, self.score
 
 
 if __name__ == "__main__":
